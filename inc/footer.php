62c863d6c2f049210cc800292a0850df0becebe3
Kieran Labarrere <web2020.kieran.labarrere@gmail.com>
2026-01-29 17:54:40 +0100

    footer.php, contact.css, main.css, img/deco, réécriture des css

--- inc/footer.php
+++ inc/footer.php
@@ -1,22 +1,30 @@
 <footer>
+
     <div class="footerNav">
+
         <nav class="navfooterbloc">
-            <!--<h3>Menu</h3>
+            <h2>Contacts</h2>
+            <address>
+                
+            <a class="maillink" href="mailto:info@wetground.be" target="_blank">info@wetground.be</a>
+
+                <a class="phonelink" href="tel:+32488191471">+32(0)488/19.14.71</a>
+                <div class="situationlink">rue de la Constitution, 13<br> 1030 Schaerbeek - Belgium</div>
+            </address>
+        </nav>
+        <nav class="navfooterbloc">
+            <h2>Menu</h2>
             <?php echo $menuMain_view ?>
-            <h3>Contacts</h3>-->
-            <a href="mailto:info@vuesurverre.be" target="_blank"><i class="fa-solid fa-envelope"></i></a>
-            <a href="https://wa.me/32488191471" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
-            <a href="callto:+32(0)488/19.14.71" target="_blank"><i class="fa-solid fa-phone"></i></a>
-            <a href="https://www.instagram.com/vue_sur_verre?igsh=MXYybm51Y3UyZGlyNg%3D%3D&utm_source=qr"
-                target="_blank"><i class="fa-brands fa-instagram"></i></a>
-            <a href="https://www.facebook.com/vue.sur.verre/" target="_blank"><i class="fab fa-facebook-f"></i></a>
         </nav>
     </div>
-    <!-- <nav id="menuRS" class="nav-rs">
-        <?php
+
+   <!-- <nav id="menuRS" class="nav-rs">
+        <?php/*
         foreach ($menuRS as $item) {
             echo "<a href=" . $item->page . " title='" . $item->titre . "' target='_blank'><div class='rs " . $item->titre . "'></div></a>";
-        }
+        }*/
         ?>
     </nav>-->
+    <img class="footer-logo" src="<?= $repImgDeco ?>logo.svg" alt="">
+
 </footer>
