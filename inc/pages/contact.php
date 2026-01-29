4af303eecbc2e54ae0d82f240c5bc81ccd29c622
Kieran Labarrere <web2020.kieran.labarrere@gmail.com>
2026-01-30 00:08:04 +0100

    contact.php, contact.css, page finalisée avec carte osm

--- inc/pages/contact.php
+++ inc/pages/contact.php
@@ -1,22 +1,32 @@
 <h2 class="invisible-titre">Contact</h2>
 
 <div class="contacts-container">
-
     <ul class="contacts">
         <li> <a class=" contacts-maillink" href="mailto:info@wetground.be">info[at]wetground.be</a>
         </li>
 
-        <li> <a class="contacts-phonelink" href="tel:+32486100573">Tel : +32486100573</a>
+        <li> <a class="contacts-phonelink" href="tel:+32486100573">Tel : +32(0)486.10.05.73</a>
         </li>
         <li> <a class="contacts-whatsapplink" aria-label="Chat on WhatsApp"
                 href="https://wa.me/+32486100573">Whatsapp</a>
         </li>
-                <li> <a class="contacts-instagram"
+        <li> <a class="contacts-instagram"
                 href="https://www.instagram.com/wetground_expo?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">Instagram</a>
         </li>
-                
+        <li>
+            <a class="contacts-adress" href="https://www.openstreetmap.org/?mlat=50.8614745&mlon=4.3692008#map=17/50.8614745/4.3692008"
+                target="_blank" rel="noopener">
+                rue de la Constitution, 13<br>
+                1030 Schaerbeek - Belgium
+            </a>
+        </li>
+
     </ul>
-    <div class="portrait-contact">
-        <img src=<?=$repDeco.'image-contact.jpg'?> alt="illustration contact">
+
+
+    <div class="map-wrapper">
+        <iframe
+            src="https://www.openstreetmap.org/export/embed.html?bbox=4.3652%2C50.8594%2C4.3732%2C50.8634&layer=mapnik&marker=50.86147450297319%2C4.3692008228386445"
+            loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </div>
