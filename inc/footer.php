224ac4692372e5de4d9d8ae170cbf13a167d6308
Kieran Labarrere <web2020.kieran.labarrere@gmail.com>
2026-01-30 01:35:18 +0100

    menu catalog en pour n'afficher que les 2 galeries concernées

--- inc/footer.php
+++ inc/footer.php
@@ -5,11 +5,14 @@
         <nav class="navfooterbloc">
             <h2>Contacts</h2>
             <address>
-                
-            <a class="maillink" href="mailto:info@wetground.be" target="_blank">info@wetground.be</a>
-
+                <a class="maillink" href="mailto:info@wetground.be" target="_blank">info@wetground.be</a>
                 <a class="phonelink" href="tel:+32488191471">+32(0)488/19.14.71</a>
-                <div class="situationlink">rue de la Constitution, 13<br> 1030 Schaerbeek - Belgium</div>
+                <a class="situationlink"
+                    href="https://www.openstreetmap.org/?mlat=50.8614745&mlon=4.3692008#map=17/50.8614745/4.3692008"
+                    target="_blank" rel="noopener">
+                    rue de la Constitution, 13<br>
+                    1030 Schaerbeek - Belgium
+                </a>
             </address>
         </nav>
         <nav class="navfooterbloc">
@@ -18,13 +21,6 @@
         </nav>
     </div>
 
-   <!-- <nav id="menuRS" class="nav-rs">
-        <?php/*
-        foreach ($menuRS as $item) {
-            echo "<a href=" . $item->page . " title='" . $item->titre . "' target='_blank'><div class='rs " . $item->titre . "'></div></a>";
-        }*/
-        ?>
-    </nav>-->
     <img class="footer-logo" src="<?= $repImgDeco ?>logo.svg" alt="">
 
 </footer>
