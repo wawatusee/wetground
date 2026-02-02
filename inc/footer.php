d38481063d72d5daef8f6fbaad1cac7b9458bb74
Kieran Labarrere <web2020.kieran.labarrere@gmail.com>
2026-02-02 07:21:32 +0100

    footer.php, model contact intégré dans footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,15 +1,15 @@
-<?php 
+<?php
 require_once '../src/model/contact_model.php';
 
 
 // 1. On charge le fichier JSON que tu viens de créer via l'admin
-$jsonPath = '../json/contacts/wetground.json'; 
+$jsonPath = '../json/contacts/wetground.json';
 $jsonRaw = file_get_contents($jsonPath);
 $data = json_decode($jsonRaw, true);
 
 // 2. Initialisation du Model et de la View
 $contactModel = new ContactModel($data);
-$d=$contactModel;
+$d = $contactModel;
 ?>
 <footer>
 
@@ -18,13 +18,11 @@ $d=$contactModel;
         <nav class="navfooterbloc">
             <h2>Contacts</h2>
 
-                        <address>
+            <address>
                 <a class="maillink" href=<?= $d->get('email') ?> target="_blank"><?= str_replace('@', '[at]', $d->get('email')) ?></a>
                 <a class="phonelink" href="tel:+32488191471">+32(0)488/19.14.71</a>
-                <a class="situationlink"
-                    href="<?= $d->get('map_url') ?>"
-                    target="_blank" rel="noopener">
-                   <?= nl2br($d->get('address', $lang)) ?>
+                <a class="situationlink" href="<?= $d->get('map_url') ?>" target="_blank" rel="noopener">
+                    <?= nl2br($d->get('address', $lang)) ?>
                 </a>
             </address>
         </nav>
--- inc/pages/contact.php
+++ inc/pages/contact.php
@@ -1,46 +1,24 @@
 <h2 class="invisible-titre">Contact</h2>
 
 <div class="contacts-container">
-    <ul class="contacts">
-        <li> <a class=" contacts-maillink" href="mailto:info@wetground.be">info[at]wetground.be</a>
-        </li>
 
-        <li> <a class="contacts-phonelink" href="tel:+32486100573">Tel : +32(0)486.10.05.73</a>
-        </li>
-        <li> <a class="contacts-whatsapplink" aria-label="Chat on WhatsApp"
-                href="https://wa.me/+32486100573">Whatsapp</a>
-        </li>
-        <li> <a class="contacts-instagram"
-                href="https://www.instagram.com/wetground_expo?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">Instagram</a>
-        </li>
-        <li>
-            <a class="contacts-adress" href="https://www.openstreetmap.org/?mlat=50.8614745&mlon=4.3692008#map=17/50.8614745/4.3692008"
-                target="_blank" rel="noopener">
-                rue de la Constitution, 13<br>
-                1030 Schaerbeek - Belgium
-            </a>
-        </li>
+    <?php
+    require_once '../src/model/contact_model.php';
+    require_once '../src/view/contact_view.php';
 
-    </ul>
-<?php 
-//TEST contact
+    // 1. On charge le fichier JSON que tu viens de créer via l'admin
+    $jsonPath = '../json/contacts/wetground.json';
+    $jsonRaw = file_get_contents($jsonPath);
+    $data = json_decode($jsonRaw, true);
 
-require_once '../src/model/contact_model.php';
-require_once '../src/view/contact_view.php';
+    // 2. Initialisation du Model et de la View
+    $contactModel = new ContactModel($data);
+    $contactView = new ContactView($contactModel, $lang);
 
-// 1. On charge le fichier JSON que tu viens de créer via l'admin
-$jsonPath = '../json/contacts/wetground.json'; 
-$jsonRaw = file_get_contents($jsonPath);
-$data = json_decode($jsonRaw, true);
+    // 3. Affichage
+    echo $contactView->render();
 
-// 2. Initialisation du Model et de la View
-$contactModel = new ContactModel($data);
-$contactView = new ContactView($contactModel, $lang);
-
-// 3. Affichage
-echo $contactView->render();
-
-?>
+    ?>
 
     <div class="map-wrapper">
         <iframe
